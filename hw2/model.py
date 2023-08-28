73e94f707e769bc6748dcbdf62264c0d71f0b2b0
Shivang <shivangsharma074@gmail.com>
2023-08-28 16:13:25 +0530

    hw2 commit

--- hw2/model.py
+++ hw2/model.py
@@ -1,6 +1,38 @@
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
+import numpy as np
+import pandas as pd
+def standardize(d,input_x):
+      means=[]
+      for i in range(d):
+          means.append(np.mean(input_x[:,i]))
+      # print(means)
+      stds=[]
+      for i in range(d):
+          std=np.std(input_x[:,i])
+          if std==0: std=1
+          stds.append(std)
+      # print(stds)
+      for i in range(d):
+          x=input_x[:,i]
+          x=(x-means[i])/stds[i]
+          # print(x)
+          input_x[:,i]=x
+      # print(input_x)
+      return input_x
+def normalize(d,input_x):
+    maxv=[]
+    minv=[]
+    for  i in range(d):
+        minv.append(np.min(input_x[:,i]))
+        maxv.append(np.max(input_x[:,i]))
+
+    for i in range(d):
+        x=input_x[:,i]
+        x=(x-minv[i])/(maxv[i]-minv[i])
+        input_x[:,i]=x
+    return input_x       
 
 class LitGenericClassifier(pl.LightningModule):
     """
@@ -15,6 +47,7 @@ class LitGenericClassifier(pl.LightningModule):
         self.lr = lr
         self.loss_func = nn.CrossEntropyLoss()
         self.model = nn.Sequential() # modify this in individual model class
+    
 
     def training_step(self, batch, batch_idx=0):
         """
@@ -50,8 +83,13 @@ class LitGenericClassifier(pl.LightningModule):
         gradient and update weights by calling `optim.step()`. You just need to return the `loss`
         appropriately. We log these values every step so that it is easier to compare various runs.
         """
-        loss = ...
-        acc = ...
+        # batch=self.transform_input(batch)
+        x,y=batch
+        pred=self.model(x)
+        loss = self.loss_func(pred,y)
+        pred=pred.argmax(dim=1)
+        temp = (pred==y).float()
+        acc=temp.mean()
         self.log('train_loss', loss.item())
         self.log('train_acc', acc)
         return loss
@@ -81,8 +119,13 @@ class LitGenericClassifier(pl.LightningModule):
           These values will be useful for you to assess overfitting and help you determine which model
         to submit on the leaderboard and in the final submission.
         """
-        loss = ...
-        acc = ...
+        # batch=self.transform_input(batch)
+        x,y=batch
+        pred=self.model(x)
+        loss = self.loss_func(pred,y)
+        pred=pred.argmax(dim=1)
+        temp = (pred==y).float()
+        acc=temp.mean()
         self.log('valid_loss', loss)
         self.log('valid_acc', acc)
         return {
@@ -116,8 +159,13 @@ class LitGenericClassifier(pl.LightningModule):
         evaluating your model. You can simply copy over the code from `validation_step` into this if 
         you wish. Just ensure that this calculation is correct.
         """
-        loss = ...
-        acc = ...
+        # batch=self.transform_input(batch)
+        x,y=batch
+        pred=self.model(x)
+        loss = self.loss_func(pred,y)
+        pred=pred.argmax(dim=1)
+        temp = (pred==y).float()
+        acc=temp.mean()
         self.log('test_loss', loss)
         self.log('test_acc', acc)
         return {
@@ -143,43 +191,61 @@ class LitGenericClassifier(pl.LightningModule):
         `y_pred`: `torch.LongTensor` of size (B,) such that `y_pred[i]` for 0 <= i < B is the label
         predicted by the classifier for `x[i]`
         """
-        y_pred = ...
-        return y_pred
+        y_pred=self.model(x)
+        # y_pred=torch.softmax
+        return y_pred.argmax(dim=1).long()
 
 class LitSimpleClassifier(LitGenericClassifier):
     def __init__(self, lr=0):
         super().__init__(lr=lr)
         self.model = nn.Sequential(
-            nn.Linear(2, ...), # d = 2
-            ..., # build your model here using `torch.nn.*` modules
-            nn.Linear(..., 4)  # num_classes = 4
+            nn.Linear(2, 100), 
+            nn.ReLU(),# d = 2
+            nn.Linear(100,16),
+            nn.ReLU(),
+            nn.Linear(16, 4)  
+            # num_classes = 4
         )
 
     def transform_input(self, batch):
-        # hardcode your transform here
-        return batch
+      input_x,input_y=batch
+
+      return input_x,input_y
 
     def configure_optimizers(self):
         # choose an optimizer from `torch.optim.*`
         # use `self.lr` to set the learning rate
         # other parameters (e.g. momentum) may be hardcoded here
-        return torch.optim.<optim_name>(...)
+        return torch.optim.Adam(params=self.parameters(),lr=self.lr)
 
 class LitDigitsClassifier(LitGenericClassifier):
     def __init__(self, lr=0):
         super().__init__(lr=lr)
         self.model = nn.Sequential(
-            nn.Linear(64, ...), # d = 64
-            ..., # build your model here using `torch.nn.*` modules
-            nn.Linear(.., 10)   # num_classes = 10
+            nn.Linear(64, 128),
+            nn.ReLU(), # d = 64
+            nn.Linear(128,256),
+            nn.ReLU(),
+            nn.Linear(256,128),
+            nn.ReLU(),
+            nn.Linear(128,16),
+            nn.ReLU(),
+            nn.Linear(16, 10)   # num_classes = 10
         )
     
     def transform_input(self, batch):
-        # hardcode your transform here
-        return batch
-
+      input_x,input_y=batch
+      # input_x=input_x.numpy()
+      #   # hardcode your transform here
+      # # mean=input_x.mean(dim=0)
+      # # std=input_x.std(dim=0)
+      # # input_x=(input_x-mean)/std
+      # input_x=standardize(64,input_x=input_x)
+      # input_x=torch.tensor(input_x,dtype=torch.float)
+      return input_x,input_y
+    
     def configure_optimizers(self):
         # choose an optimizer from `torch.optim.*`
         # use `self.lr` to set the learning rate
         # other parameters (e.g. momentum) may be hardcoded here
-        return torch.optim.<optim_name>(...)
+        return torch.optim.Adam(params=self.parameters(),lr=self.lr,weight_decay=1e-2)
